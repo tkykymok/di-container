b2da246753209086a98ae78dc81bef9416580500
Matsumoto Masayoshi <matsumoto.develop@gmail.com>
2020-11-15 16:48:09 +0900

    first commit

---
+++ src/main/java/com/sample/di_container/App.java
@@ -0,0 +1,13 @@
+package com.sample.di_container;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
