4b3396257566b276b33d4978fc6a6d41e26b4a98
Matsumoto Masayoshi <matsumoto.develop@gmail.com>
2020-11-15 18:19:21 +0900

    オブジェクトプールを作成、オブジェクトプールからインスタンスを取得できるように修正

--- src/main/java/com/sample/di_container/Application.java
+++ src/main/java/com/sample/di_container/Application.java
@@ -1,5 +1,9 @@
 package com.sample.di_container;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import com.sample.di_container.application.AdvertiseService;
 import com.sample.di_container.application.product.ProductRepository;
 import com.sample.di_container.application.product.ProductRepositoryImpl;
@@ -12,10 +16,33 @@ import com.sample.di_container.application.tweet.TwitterAdapterImpl;
  */
 public class Application {
     public static void main(String[] args) {
-        ProductRepository productRepository = new ProductRepositoryImpl();
-        TwitterAdapter twitterAdapter = new TwitterAdapterImpl();
+        Context.register("productRepository", ProductRepositoryImpl.class);
+        Context.register("twitterAdapter", TwitterAdapterImpl.class);
 
-        AdvertiseService advertiseService = new AdvertiseService(productRepository, twitterAdapter);
+        AdvertiseService advertiseService = new AdvertiseService(
+                (ProductRepository) Context.getBean("productRepository"),
+                (TwitterAdapter) Context.getBean("twitterAdapter"));
         advertiseService.advertise(1, "販促メッセージ");
+    }
+
+    static class Context {
+        static Map<String, Class> types = new HashMap<>();
+        static Map<String, Object> beans = new HashMap<>();
+
+        static void register(String name, Class type) {
+            types.put(name, type);
+        }
+
+        static Object getBean(String name) {
+            return beans.computeIfAbsent(name, key -> {
+                Class type = types.get(name);
+                Objects.requireNonNull(type, name + " not found.");
+                try {
+                    return type.newInstance();
+                } catch (InstantiationException | IllegalAccessException ex) {
+                    throw new RuntimeException(name + " can not instanciate", ex);
+                }
+            });
+        }
     }
 }
